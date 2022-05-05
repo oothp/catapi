26f1d08dfa68665963fdc808397885ad7c580f6d
Miglé <oakeh@tuta.io>
2022-05-05 17:35:58 +0300

    is author

--- models/review.js
+++ models/review.js
@@ -23,7 +23,8 @@ const ReviewSchema = new mongoose.Schema({
     },
     comments: [ Comment ],
     photos: [ String ],
-    created_at: { type: String, default: Date.now }
+    created_at: { type: String, default: Date.now },
+    is_author: { type: Boolean, default: false } // for deleting/editing reviews?
 })
 
 ReviewSchema.methods.toJSON = function() {
