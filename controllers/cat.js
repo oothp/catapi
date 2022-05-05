a3f5085c1411f075002325b6c62d3ebb8531c5e6
Miglé <oakeh@tuta.io>
2022-05-05 17:34:58 +0300

    cat service

--- controllers/cat.js
+++ controllers/cat.js
@@ -1,107 +1,38 @@
-const mongoose = require("mongoose"), Schema = mongoose.Schema; 
+const service = require('../service/cat_service')
 
 //GET '/cats'
 const getAllCats = (req, res) => {
-    Cat.find({}, (err, data) => {
-        if (err) {
-            return res.json({ Error: err });
-        }
-        return res.json(data);
-        });
-  // res.json({message: "GET all cats"});
-};
+    service.getAll()
+        .then(cats => { res.status(200).send(cats) })
+        .catch(err => {
+            console.log(err)
+            sendError(res, err)
+        })
+}
 
-//POST cat
-const newCat = (req, res) => {
-    console.log('NewCatttt => req ' + req.body);
-  //check if the cat already exists in db
-    // Cat.findOne({ name: req.body.name }, (err, data) => {
-    // //if cat not in db, add it
-    //     if (!data) {
-        //create a new tea object using the Cta model and req.body
-            const newCat = new Cat({
-                _id: mongoose.Types.ObjectId(),
-                name: req.body.name,
-                description: req.body.description,
-                will_scratch: req.body.will_scratch,
-                created_at: req.body.created_at
-            // locations: req.body.locations,
-            });
-
-        // save this object to database
-            newCat.save((err, data) => {
-            if (err) return res.json({ Error: data + " + " + err});
-            return res.json(data);
-            });
-        //if there's an error or the tea is in db, return a message
-        // } else {
-            // if (err)
-            // return res.json(`Something went wrong, please try again. ${err}`);
-            // return res.json({ message: "Cat already exists" });
-        // }
-    // }
-    // );
-};
-
-//GET '/cat/:name'
-// const getCatByName = (req, res) => {
-//     let _name = req.params.name; //get the cat name
-//     // let id = req.params.name;
-
-//     //find the specific tea with that name
-//     Cat.findOne({name:_name}, (err, data) => {
-//     if(err || !data) {
-//         return res.json({message: "Cat name not found."});
-//     }
-//     else return res.json(data); //return the cat object if found
-//     });
-//     // res.json({ message: "GET 1 cat" });
-// };
-
-//GET '/cat/id'
+//GET '/cat/:id'
 const getCatById = (req, res) => {
-    let _id = req.params.id; //get the cat name
-    // let id = req.params.name;
+    service.getCatById(req.params)
+        .then(cat => { res.status(200).send(cat) })
+        .catch(err => {
+            console.error(err)
+            sendError(res, err)
+        })
+}
 
-    //find the specific tea with that name
-    Cat.findOne({id:_id}, (err, data) => {
-    if(err || !data) {
-        return res.json({message: "Cat id not found."});
-    }
-    else return res.json(data); //return the cat object if found
-    });
-    // res.json({ message: "GET 1 cat" });
-};
-
-//POST '/cat/:name'
-const newCatPhoto = (req, res, next) => {
-    res.json({ message: "POST 1 cat photo" });
-};
+//POST '/cat/:id'
+// const newCatPhoto = (req, res, next) => {
+//     res.json({ message: "POST 1 cat photo" });
+// }
 
 //DELETE '/cat/:name'
-const deleteCat = (req, res) => {
-    let name = req.params.name; // get the id of review to delete
-
-    Cat.deleteOne({name:name}, (err, data) => {
-    //if there's nothing to delete return a message
-    if( data.deletedCount == 0) return res.json({message: "cat doesn't exist."});
-    //else if there's an error, return the err message
-    else if (err) return res.json(`Something went wrong, please try again. ${err}`);
-    //else, return the success message
-    else return res.json({message: "cat deleted."});
-    });
-    // res.json({ message: "DELETE 1 cat" });
-};
+// cant delete cats
 
 // //export controller functions
 module.exports = {
     getAllCats,
-    newCat,
-    // deleteAllCats,
-    // getCatByName,
     getCatById,
-    newCatPhoto,
-    deleteCat,
-};
+    // newCatPhoto,
+}
 
 const Cat = require("../models/cat");
